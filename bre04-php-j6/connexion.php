48b554dc934f794074669179b2d23129349b1ab6
manons56 <manon.sara@3wa.io>
2025-06-11 09:01:28 +0000

    étape 1 : connexion

---
+++ bre04-php-j6/connexion.php
@@ -0,0 +1,19 @@
+<?php
+
+$host = "db.3wa.io";
+$port = "3306";
+$dbname = "manonsara_phpj6";
+$connexionString = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8";
+
+$user = "manonsara";
+$password = "7639763c730e9069300e065f0a99cd54";
+
+$db = new PDO(
+    $connexionString,
+    $user,
+    $password
+);
+
+var_dump($db);
+
+?>
