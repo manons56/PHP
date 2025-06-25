be7846f74e6e406c7da6dc8693d7676fb7e4a2ba
manons56 <manon.sara@3wa.io>
2025-06-25 13:15:06 +0000

    bre04-php-exercice-crud-mvc

--- bre04-php-exercice-crud-mvc/config/autoload.php
+++ bre04-php-exercice-crud-mvc/config/autoload.php
@@ -3,3 +3,4 @@
 require_once "controllers/UserController.php";
 require_once "config/Router.php";
 require_once "models/User.php";
+require_once "managers/AbstractManager.php";
---
+++ bre04-php-exercice-crud-mvc/managers/AbstractManager.php
@@ -0,0 +1,18 @@
+
+<?php
+
+abstract class AbstractManager
+{
+    protected PDO $db;
+
+    public function __construct()
+    {
+       
+            $this->db = new PDO(
+                'mysql:host=db.3wa.io;port ="3306";dbname=manonsara_crud_mvc;',
+                'manonsara',
+                '7639763c730e9069300e065f0a99cd54'
+            );
+
+    }
+}
