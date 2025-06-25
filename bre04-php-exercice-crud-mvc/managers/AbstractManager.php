cddbe193b30e4529f54cf48a5a1f2ef9acf34eb4
manons56 <manon.sara@3wa.io>
2025-06-25 13:38:45 +0000

    bre04-php-exercice-crud-mvc

--- bre04-php-exercice-crud-mvc/managers/AbstractManager.php
+++ bre04-php-exercice-crud-mvc/managers/AbstractManager.php
@@ -7,12 +7,20 @@ abstract class AbstractManager
 
     public function __construct()
     {
-       
-            $this->db = new PDO(
-                'mysql:host=db.3wa.io;port =3306;dbname=manonsara_crud_mvc;',
-                'manonsara',
-                '7639763c730e9069300e065f0a99cd54'
-            );
-
-    }
+        $host = "db.3wa.io";
+        $port = "3306";
+        $dbname = "manonsara_crud_mvc";
+        $connexionString = "mysql:host=$host;port=$port;dbname=$dbname";
+        
+        $user = "manonsara";
+        $password = "7639763c730e9069300e065f0a99cd54";
+        
+        $this -> db = new PDO(
+            $connexionString,
+            $user,
+            $password
+              
+        }
 }
+
+
